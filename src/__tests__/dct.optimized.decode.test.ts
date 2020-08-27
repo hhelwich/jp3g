e8bf552da15b47b47fbbdee95d48f2ce96d7b751
Hendrik Helwich <hendrik@helwich.de>
2020-08-28 01:30:57 +0200

    Extract 1/8

--- src/__tests__/dct.optimized.decode.test.ts
+++ src/__tests__/dct.optimized.decode.test.ts
@@ -4,7 +4,7 @@ import { idct } from '../dct.optimized.decode'
 
 describe('IDCT (optimized)', () => {
   it('equals result of reference IDCT', () => {
-    const precision = 11
+    const precision = 10
     const g = idctReference(G).map(x => +x.toFixed(precision))
     const g2 = idct(G).map(x => +x.toFixed(precision))
     expect(g).toEqual(g2)
--- src/dct.optimized.decode.ts
+++ src/dct.optimized.decode.ts
@@ -1,6 +1,19 @@
-import { mult8x8, transp8x8, M } from './dct.naive-matrix.encode'
+import { mult8x8, transp8x8 } from './dct.naive-matrix.encode'
+const { cos, PI } = Math
+
+export const M = (() => {
+  const M: number[] = []
+  for (let i = 0; i < 8; i += 1) {
+    for (let j = 0; j < 8; j += 1) {
+      M[i * 8 + j] =
+        i === 0 ? 1 : Math.sqrt(2) * cos(((2 * j + 1) * i * PI) / 16)
+    }
+  }
+  return M
+})()
 
 /**
  * TODO: optimize
  */
-export const idct = (T: number[]) => mult8x8(transp8x8(M), mult8x8(T, M))
+export const idct = (T: number[]) =>
+  mult8x8(transp8x8(M), mult8x8(T, M)).map(x => x / 8)
