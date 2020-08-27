34010f67b8bca59c752c28aebb995c9d5999105a
Hendrik Helwich <hendrik@helwich.de>
2020-08-27 22:30:22 +0200

    Don't nest matrix array

--- src/dct.naive.ts
+++ src/dct.naive.ts
@@ -4,24 +4,23 @@ const { cos, PI, SQRT2 } = Math
 
 const a = (u: number) => (u === 0 ? 1 / SQRT2 : 1)
 
-const G = (g: number[][], u: number, v: number) => {
+const G = (g: number[], u: number, v: number) => {
   let sum = 0
   for (let x = 0; x < 8; x += 1) {
     for (let y = 0; y < 8; y += 1) {
       sum +=
-        g[x][y] *
+        g[x * 8 + y] *
         cos(((2 * x + 1) * u * PI) / 16) *
         cos(((2 * y + 1) * v * PI) / 16)
     }
   }
   return 0.25 * a(u) * a(v) * sum
 }
-export const dct = (g: number[][]) => {
-  let result: number[][] = []
+export const dct = (g: number[]) => {
+  let result: number[] = []
   for (let u = 0; u < 8; u += 1) {
-    result[u] = []
     for (let v = 0; v < 8; v += 1) {
-      result[u][v] = G(g, u, v)
+      result[u * 8 + v] = G(g, u, v)
     }
   }
   return result
