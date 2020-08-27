2458964afe046b5662edf80c15e1c4b808718c6c
Hendrik Helwich <hendrik@helwich.de>
2020-08-27 22:20:53 +0200

    Add naive DCT implementation

---
+++ src/dct.naive.ts
@@ -0,0 +1,28 @@
+const { cos, PI, SQRT2 } = Math
+
+// See: https://en.wikipedia.org/wiki/JPEG#Discrete_cosine_transform
+
+const a = (u: number) => (u === 0 ? 1 / SQRT2 : 1)
+
+const G = (g: number[][], u: number, v: number) => {
+  let sum = 0
+  for (let x = 0; x < 8; x += 1) {
+    for (let y = 0; y < 8; y += 1) {
+      sum +=
+        g[x][y] *
+        cos(((2 * x + 1) * u * PI) / 16) *
+        cos(((2 * y + 1) * v * PI) / 16)
+    }
+  }
+  return 0.25 * a(u) * a(v) * sum
+}
+export const dct = (g: number[][]) => {
+  let result: number[][] = []
+  for (let u = 0; u < 8; u += 1) {
+    result[u] = []
+    for (let v = 0; v < 8; v += 1) {
+      result[u][v] = G(g, u, v)
+    }
+  }
+  return result
+}
