b106b48d33db8cd18993f9917f439973856eb0cb
Hendrik Helwich <hendrik@helwich.de>
2020-08-30 18:16:15 +0200

    Optimize indices

--- src/dct.optimized.encode.ts
+++ src/dct.optimized.encode.ts
@@ -5,15 +5,15 @@ const { SQRT2 } = Math
  * Calculate B := (M/sqrt(8) * A)^t
  */
 const multM = (A: number[], B: number[]) => {
-  for (let i = 0; i < 8; i += 1) {
-    const a0 = A[0 * 8 + i]
-    const a1 = A[1 * 8 + i]
-    const a2 = A[2 * 8 + i]
-    const a3 = A[3 * 8 + i]
-    const a4 = A[4 * 8 + i]
-    const a5 = A[5 * 8 + i]
-    const a6 = A[6 * 8 + i]
-    const a7 = A[7 * 8 + i]
+  for (let j = 0, i = 0; j < 8; j += 1) {
+    const a0 = A[j]
+    const a1 = A[j + 8]
+    const a2 = A[j + 16]
+    const a3 = A[j + 24]
+    const a4 = A[j + 32]
+    const a5 = A[j + 40]
+    const a6 = A[j + 48]
+    const a7 = A[j + 56]
     const g0 = a0 + a7
     const g1 = a1 + a6
     const g2 = a2 + a5
@@ -32,14 +32,14 @@ const multM = (A: number[], B: number[]) => {
     const c5 = g4 + d5
     const c6 = g7 - d6
     const c7 = d6 + g7
-    B[i * 8 + 0] = f0 + f1
-    B[i * 8 + 1] = m7 * c4 + m1 * c7
-    B[i * 8 + 2] = m6 * f2 + m2 * f3
-    B[i * 8 + 3] = m3 * c6 - m5 * c5
-    B[i * 8 + 4] = f0 - f1
-    B[i * 8 + 5] = m3 * c5 + m5 * c6
-    B[i * 8 + 6] = m6 * f3 - m2 * f2
-    B[i * 8 + 7] = m7 * c7 - m1 * c4
+    B[i++] = f0 + f1
+    B[i++] = m7 * c4 + m1 * c7
+    B[i++] = m6 * f2 + m2 * f3
+    B[i++] = m3 * c6 - m5 * c5
+    B[i++] = f0 - f1
+    B[i++] = m3 * c5 + m5 * c6
+    B[i++] = m6 * f3 - m2 * f2
+    B[i++] = m7 * c7 - m1 * c4
   }
 }
 
