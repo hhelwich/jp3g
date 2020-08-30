a659e041c34741e3118811d48e839c7aa182e910
Hendrik Helwich <hendrik@helwich.de>
2020-08-30 22:15:17 +0200

    Use best approximations

--- src/dct.optimized.common.ts
+++ src/dct.optimized.common.ts
@@ -2,9 +2,9 @@
 export const m1 = 1.3870398453221475
 export const m2 = 1.3065629648763766
 export const m3 = 1.1758756024193588
-export const m5 = 0.7856949583871023
-export const m6 = 0.5411961001461971
-export const m7 = 0.2758993792829431
+export const m5 = 0.7856949583871022
+export const m6 = 0.541196100146197
+export const m7 = 0.275899379282943
 
 const tmp: number[] = []
 
