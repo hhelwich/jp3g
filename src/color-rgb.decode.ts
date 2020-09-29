10a93bf365c7192e80ca705fa1847edd41391c48
Hendrik Helwich <hendrik@helwich.de>
2020-09-29 23:27:24 +0200

    Expand formula

--- src/color-rgb.decode.ts
+++ src/color-rgb.decode.ts
@@ -8,8 +8,7 @@ export const yCbCr2Rgb = ([Y, Cb, Cr]: [number, number, number]): [
   number,
   number
 ] => [
-  // TODO Expand
-  Y + 1.402 * (Cr - 128),
-  Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128),
-  Y + 1.772 * (Cb - 128),
+  Y + 1.402 * Cr - 179.456,
+  Y - 0.34414 * Cb - 0.71414 * Cr + 135.45984,
+  Y + 1.772 * Cb - 226.816,
 ]
