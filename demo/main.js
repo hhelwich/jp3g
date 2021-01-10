9c4482991db4ee3af9056961fcefdaa87007f0fe
Hendrik Helwich <hendrik@helwich.de>
2021-01-10 18:59:45 +0100

    Add working demo

--- demo/main.js
+++ demo/main.js
@@ -1,21 +1,91 @@
-;(async () => {
-  try {
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-
-    console.log(`Using jp3g ${jp3g.version}`)
-
-    if (window.Worker) {
-      jp3g.setWorkers(new Worker('../dist/jp3g.js'))
+/// <reference path="../dist/types/index.d.ts" />
+var showImage = function (file, main, downScale, callback) {
+    jp3g(file)
+        .scale(1 / downScale)
+        .toImageData(function (error, imageData) {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        var canvas = document.createElement('canvas');
+        canvas.width = imageData.width;
+        canvas.height = imageData.height;
+        main.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
+        ctx.putImageData(imageData, 0, 0);
+        callback(null, null);
+    });
+};
+var imageCount = 0;
+var queuedImageCount = 0;
+var draw = function () {
+    var canvas = document.getElementById('loader');
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    var size = Math.min(width, height);
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#3498db';
+    ctx.translate(width / 2, height / 2);
+    var time = new Date();
+    ctx.rotate(((10 * Math.PI) / 60) * time.getSeconds() +
+        ((10 * Math.PI) / 60000) * time.getMilliseconds());
+    ctx.beginPath();
+    ctx.lineTo(0, 0);
+    ctx.arc(0, 0, size / 2, 0, Math.PI * 1.8 * (queuedImageCount / imageCount));
+    ctx.closePath();
+    ctx.fill();
+    requestAnimationFrame(draw);
+};
+var workerCount = 0;
+var downScale = 8;
+var $files = document.getElementById('files');
+var $main = document.getElementsByTagName('main')[0];
+var $workerCount = document.getElementById('workerCount');
+var $downScale = document.getElementById('downScale');
+var setWorkerCount = function () {
+    workerCount = +$workerCount.value;
+    jp3g.setWorkerCount(workerCount);
+};
+$workerCount.addEventListener('change', setWorkerCount);
+if (!window.Worker) {
+    $workerCount.disabled = true;
+    $workerCount.value = '0';
+    //$workerCount.dispatchEvent(new Event('input'))
+}
+else {
+    $workerCount.value = "" + workerCount;
+    //$workerCount.dispatchEvent(new Event('input'))
+}
+setWorkerCount();
+$downScale.addEventListener('change', function () {
+    downScale = +$downScale.value;
+});
+$downScale.value = "" + downScale;
+//$downScale.dispatchEvent(new Event('input'))
+$files.addEventListener('change', function () {
+    var files = $files.files;
+    $main.innerHTML = '';
+    queuedImageCount += files.length;
+    imageCount = queuedImageCount;
+    var _loop_1 = function (i) {
+        var file = files[i];
+        jp3g.waitIdle(function () {
+            showImage(file, $main, downScale, function (error) {
+                if (error) {
+                    console.error(error);
+                }
+                queuedImageCount -= 1;
+                if (queuedImageCount === 0) {
+                    imageCount = 0;
+                }
+            });
+        });
+    };
+    for (var i = 0; i < files.length; i += 1) {
+        _loop_1(i);
     }
-
-    const jpegData = await (await fetch('image.jpg')).arrayBuffer()
-    const jpeg = await jp3g.decode(jpegData)
-    const imageData = await jp3g.decodeImage(jpeg)
-    canvas.width = imageData.width
-    canvas.height = imageData.height
-    ctx.putImageData(imageData, 0, 0)
-  } catch (e) {
-    console.error('error', e)
-  }
-})()
+}, false);
+console.log("Using jp3g " + jp3g.version);
+requestAnimationFrame(draw);
---
+++ demo/main.ts
@@ -0,0 +1,112 @@
+/// <reference path="../dist/types/index.d.ts" />
+
+type Callback<T> = (error: Error | undefined | null, result: T) => void
+
+const showImage = (
+  file: File,
+  main: HTMLElement,
+  downScale: number,
+  callback: Callback<void>
+) => {
+  jp3g(file)
+    .scale(1 / downScale)
+    .toImageData((error, imageData) => {
+      if (error) {
+        callback(error, null)
+        return
+      }
+      const canvas = document.createElement('canvas')
+      canvas.width = imageData.width
+      canvas.height = imageData.height
+      main.appendChild(canvas)
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+      ctx.putImageData(imageData, 0, 0)
+      callback(null, null)
+    })
+}
+
+let imageCount = 0
+let queuedImageCount = 0
+
+const draw = () => {
+  const canvas = document.getElementById('loader') as HTMLCanvasElement
+  const width = canvas.clientWidth
+  const height = canvas.clientHeight
+  canvas.width = width
+  canvas.height = height
+  const size = Math.min(width, height)
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+  ctx.fillStyle = '#3498db'
+  ctx.translate(width / 2, height / 2)
+  const time = new Date()
+  ctx.rotate(
+    ((10 * Math.PI) / 60) * time.getSeconds() +
+      ((10 * Math.PI) / 60000) * time.getMilliseconds()
+  )
+  ctx.beginPath()
+  ctx.lineTo(0, 0)
+  ctx.arc(0, 0, size / 2, 0, Math.PI * 1.8 * (queuedImageCount / imageCount))
+  ctx.closePath()
+  ctx.fill()
+  requestAnimationFrame(draw)
+}
+
+let workerCount = 0
+let downScale = 8
+
+const $files = document.getElementById('files') as HTMLInputElement
+const $main = document.getElementsByTagName('main')[0]
+const $workerCount = document.getElementById('workerCount') as HTMLInputElement
+const $downScale = document.getElementById('downScale') as HTMLInputElement
+
+const setWorkerCount = () => {
+  workerCount = +$workerCount.value
+  jp3g.setWorkerCount(workerCount)
+}
+
+$workerCount.addEventListener('change', setWorkerCount)
+if (!window.Worker) {
+  $workerCount.disabled = true
+  $workerCount.value = '0'
+  //$workerCount.dispatchEvent(new Event('input'))
+} else {
+  $workerCount.value = `${workerCount}`
+  //$workerCount.dispatchEvent(new Event('input'))
+}
+setWorkerCount()
+
+$downScale.addEventListener('change', () => {
+  downScale = +$downScale.value
+})
+$downScale.value = `${downScale}`
+//$downScale.dispatchEvent(new Event('input'))
+
+$files.addEventListener(
+  'change',
+  () => {
+    const files = $files.files
+    $main.innerHTML = ''
+    queuedImageCount += files.length
+    imageCount = queuedImageCount
+
+    for (let i = 0; i < files.length; i += 1) {
+      const file = files[i]
+      jp3g.waitIdle(() => {
+        showImage(file, $main, downScale, error => {
+          if (error) {
+            console.error(error)
+          }
+          queuedImageCount -= 1
+          if (queuedImageCount === 0) {
+            imageCount = 0
+          }
+        })
+      })
+    }
+  },
+  false
+)
+
+console.log(`Using jp3g ${jp3g.version}`)
+
+requestAnimationFrame(draw)
